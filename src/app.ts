8b673ba31e267977e0b6d10a28aff8efb653d574
Nahuel De La Cruz <n_p4nic@hotmail.com>
2024-07-30 19:37:27 -0300

    Integración de webhook de discord y github

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,15 @@
-console.log('Hola mundo');
+import express, { Request, Response } from 'express';
+import { envs } from './config';
+import { GithubController } from './presentation/github/controller';
+
+(()=>{
+  main();
+})();
+
+function main() {
+  const app = express();
+  const controller = new GithubController();
+  app.use(express.json());
+  app.listen(envs.PORT,()=>console.log(`Server running on: ${envs.PORT}`));
+  app.post('/api/github',controller.webhookHandler);
+};
